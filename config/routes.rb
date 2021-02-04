d8e7d680f75988b8b0574b0d085591693aebcb43
KKe-coder <carumag726@gmail.com>
2021-02-04 10:21:45 +0000

    [Add] 2021-02-04エラーメッセージ分岐記述

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :posts
+  get "titleblank", :to => "posts#titleblank"
+  get "titletoolong", :to => "posts#titletoolong"
+  get "bodyblank", :to => "posts#bodyblank"
 end
